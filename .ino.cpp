a32155ca0cb5085bead15a8ca36fa60537ccaddd
Laurenz <github@laure.nz>
2016-09-10 21:17:21 +0200

    Add WiFi code and move font to separate file

--- .ino.cpp
+++ .ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2016-09-10 20:26:15
+//This file has been generated on 2016-09-10 21:15:14
 
 #include "Arduino.h"
 #include <adafruit_feather.h>
@@ -13,14 +13,17 @@
 #include <SPI.h>
 #include <map>
 #include <vector>
+#include "font.h"
 void setupSensor() ;
+bool connectAP(void) ;
+void send_msg_via_udp() ;
 char char_to_index(char c) ;
 void setup() ;
 void setChar(char c, char col, int *rgb) ;
 void clearChar() ;
 void loop() ;
 
-
 #include "BlinkenFlights.ino"
+
 
 #endif
--- BlinkenFlights.ino
+++ BlinkenFlights.ino
@@ -1,4 +1,3 @@
-
 #include <adafruit_feather.h>
 #include <Adafruit_TLC59711.h>
 #include <Adafruit_Sensor.h>
@@ -10,6 +9,8 @@
 #include <map>
 #include <vector>
 
+#include "font.h"
+
 #define WLAN_SSID   "AndroidAP"
 #define WLAN_PASS   "blinkblink"
 /// TODO: get from hostname
@@ -19,7 +20,6 @@ AdafruitUDP udp;
 const uint16_t local_port = 88;
 bool with_serial = true;
 char packetBuffer[255];
-
 
 Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0();
 
@@ -32,161 +32,195 @@ float ax, ay, az;
 float a_norm;
 
 int gyro_z_offset = 8;
+long last_heartbeat = 0;
 
-void setupSensor()
-{
-  // 1.) Set the accelerometer range
-  lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_2G);
-  mg_per_lsb_accel = LSM9DS0_ACCEL_MG_LSB_2G/1000.0;
-  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_4G);
-  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_6G);
-  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_8G);
-  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_16G);
+void setupSensor() {
+	// 1.) Set the accelerometer range
+	lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_2G);
+	mg_per_lsb_accel = LSM9DS0_ACCEL_MG_LSB_2G / 1000.0;
+	//lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_4G);
+	//lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_6G);
+	//lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_8G);
+	//lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_16G);
 
-  // 2.) Set the magnetometer sensitivity
-  lsm.setupMag(lsm.LSM9DS0_MAGGAIN_2GAUSS);
-  //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_4GAUSS);
-  //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_8GAUSS);
-  //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_12GAUSS);
+	// 2.) Set the magnetometer sensitivity
+	lsm.setupMag(lsm.LSM9DS0_MAGGAIN_2GAUSS);
+	//lsm.setupMag(lsm.LSM9DS0_MAGGAIN_4GAUSS);
+	//lsm.setupMag(lsm.LSM9DS0_MAGGAIN_8GAUSS);
+	//lsm.setupMag(lsm.LSM9DS0_MAGGAIN_12GAUSS);
 
-  // 3.) Setup the gyroscope
-  // lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_245DPS);
-  // dps_per_lsb_gyro = LSM9DS0_GYRO_DPS_DIGIT_245DPS;
+	// 3.) Setup the gyroscope
+	// lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_245DPS);
+	// dps_per_lsb_gyro = LSM9DS0_GYRO_DPS_DIGIT_245DPS;
 
-  //lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_500DPS);
-  // dps_per_lsb_gyro = LSM9DS0_GYRO_DPS_DIGIT_500DPS;
+	//lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_500DPS);
+	// dps_per_lsb_gyro = LSM9DS0_GYRO_DPS_DIGIT_500DPS;
 
-  lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_2000DPS);
-  dps_per_lsb_gyro = LSM9DS0_GYRO_DPS_DIGIT_2000DPS;
+	lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_2000DPS);
+	dps_per_lsb_gyro = LSM9DS0_GYRO_DPS_DIGIT_2000DPS;
 }
 
-char text[][5 * 5] = { { 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0,
-    0, 1, 1, 0, 0, 0, 1 }, { 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1,
-    0, 0, 0, 1, 1, 1, 1, 1, 0 }, { 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0,
-    0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0 }, { 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0,
-    0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0 }, { 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-    1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1, 1, 0, 0,
-    0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0 }, { 0, 1, 1, 1, 1, 1,
-    0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0 }, { 1, 0, 0, 0,
-    1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1 }, { 0, 0,
-    1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0 }, {
-    0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0,
-    0 }, { 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0,
-    0, 1, 0 }, { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0,
-    1, 1, 1, 1, 1 }, { 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0,
-    0, 1, 1, 0, 0, 0, 1 }, { 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1,
-    0, 0, 1, 1, 1, 0, 0, 0, 1 }, { 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0,
-    1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0 }, { 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1,
-    1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0 }, { 0, 1, 1, 1, 0, 1, 0, 0, 1, 0,
-    1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1 }, { 1, 1, 1, 1, 0, 1, 0, 0,
-    0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0 }, { 0, 1, 1, 1, 1, 1,
-    0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0 }, { 1, 1, 1, 1,
-    1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0 }, { 1, 0,
-    0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0 }, {
-    1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0,
-    0 }, { 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0,
-    0, 0, 1 }, { 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0,
-    1, 0, 0, 0, 1 }, { 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1,
-    0, 0, 0, 0, 1, 0, 0 }, { 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0,
-    1, 0, 0, 0, 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0,
-    0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0,
-    1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0 } };
+bool connectAP(void)
+{
+  // Attempt to connect to an AP
+if (with_serial)
+{
+  Serial.print("Please wait while connecting to: '" WLAN_SSID "' ... ");
+}
+  if ( Feather.connect(WLAN_SSID, WLAN_PASS) )
+  {
+    if (with_serial) {Serial.println("Connected!");}
+  }
+  else
+  {
+    if (with_serial){Serial.printf("Failed! %s (%d)", Feather.errstr(), Feather.errno());}
+    if (with_serial){Serial.println();}
+  }
+  if (with_serial){Serial.println();}
+
+  return Feather.connected();
+}
+
+void send_msg_via_udp()
+{
+  udp.beginPacket(server_ip, port);
+  udp.println(msg); // send with newline
+  udp.endPacket();
+  last_heartbeat = millis();
+}
+
+
 
 char char_to_index(char c) {
-  if (c >= 'A' && c <= 'Z')
-    return c - 'A';
-  if (c == ':')
-    return 27;
-  if (c == ')')
-    return 28;
-  return 26;
+	if (c >= 'A' && c <= 'Z')
+		return c - 'A';
+	if (c == ':')
+		return 27;
+	if (c == ')')
+		return 28;
+	return 26;
 }
 Adafruit_TLC59711 tlc = Adafruit_TLC59711(2); // two daisy chained boards
 
 void setup() {
-  setupSensor();
-  Wire.begin();
-  tlc.begin();
-  pinMode(BOARD_LED_PIN, OUTPUT);
-  Serial.begin(9600);
-  Serial.println("BLINKENFLIGHTS!!!11!");
+	pinMode(BOARD_LED_PIN, OUTPUT);
+
+	Serial.begin(9600);
+	Serial.println("BLINKENFLIGHTS!!!11!");
+
+	while ( !connectAP() )
+	{
+		digitalWrite(BOARD_LED_PIN, HIGH);
+		delay(50); // delay between each attempt
+		digitalWrite(BOARD_LED_PIN, LOW);
+		delay(50);
+	}
+	udp.begin(local_port);
+
+	setupSensor();
+	Wire.begin();
+	// tlc.begin();
+
+	digitalWrite(BOARD_LED_PIN, HIGH);
 }
 
 void setChar(char c, char col, int *rgb) {
-  for (int row = 0; row < 5; row++) {
-    int led_nr = 5 - row;
-    if (text[char_to_index(c)][5 * row + col]) {
-      tlc.setLED(led_nr, rgb[0], rgb[1], rgb[2]);
-    } else {
-      tlc.setLED(led_nr, 0, 0, 0);
-    }
-  }
-  tlc.write();
+	for (int row = 0; row < 5; row++) {
+		int led_nr = 5 - row;
+		if (text[char_to_index(c)][5 * row + col]) {
+			tlc.setLED(led_nr, rgb[0], rgb[1], rgb[2]);
+		} else {
+			tlc.setLED(led_nr, 0, 0, 0);
+		}
+	}
+	tlc.write();
 }
 
 void clearChar() {
-  for (int row = 0; row < 6; row++) {
-    tlc.setLED(row, 0, 0, 0);
-  }
-  tlc.write();
+	for (int row = 0; row < 6; row++) {
+		tlc.setLED(row, 0, 0, 0);
+	}
+	tlc.write();
 }
 
-int blue[] =  { 0, 0, 0x1000 };
-int green[] =   { 0x1000, 0x1000, 0 };
-int red[] =   { 0x1000, 0, 0 };
-int cyan[] =  { 0, 0x1000, 0x1000 };
+int blue[] = { 0, 0, 0x1000 };
+int green[] = { 0x1000, 0x1000, 0 };
+int red[] = { 0x1000, 0, 0 };
+int cyan[] = { 0, 0x1000, 0x1000 };
 int magenta[] = { 0x1000, 0, 0x1000 };
-int yellow[] =  { 0x1000, 0x1000, 0 };
+int yellow[] = { 0x1000, 0x1000, 0 };
 
 void loop() {
-  lsm.read();
-  ax = lsm.accelData.x*mg_per_lsb_accel;
-  ay = lsm.accelData.y*mg_per_lsb_accel;
-  az = lsm.accelData.z*mg_per_lsb_accel;
 
-  a_norm = sqrt(ax*ax+ay*ay+az*az);
+	long now = millis();
+	if (now - last_heartbeat > 1000) {
+		sprintf(msg, "Heartbeat: %i", last_heartbeat);
+		send_msg_via_udp(); //// sends global msg-variable
+		last_heartbeat = now;
 
-  gx = (int)lsm.gyroData.x*dps_per_lsb_gyro;
-  gy = (int)lsm.gyroData.y*dps_per_lsb_gyro;
-  gz = (int)lsm.gyroData.z*dps_per_lsb_gyro + gyro_z_offset; // in deg/s
+		digitalWrite(BOARD_LED_PIN, HIGH);
+		delay(50); // delay between each attempt
+		digitalWrite(BOARD_LED_PIN, LOW);
+		delay(50);
+	}
+return;
+	int packetSize = udp.parsePacket();
 
+	if (packetSize) {
+		sprintf(msg, "Received UDP: %i Byte", packetSize);
+		send_msg_via_udp(); //// sends global msg-variable
+		udp.read(packetBuffer, sizeof(packetBuffer)); // char[]
+		if (packetBuffer[0] == 'L') {
+			sprintf(msg, "LAUFLICHT: %i, %s", packetSize, packetBuffer);
+			send_msg_via_udp(); //// sends global msg-variable
+		}
 
-  char mystring[] = "TECHFEST";
+	}
 
-//  std::vector<int*> colors = {blue, green, red, cyan, magenta, yellow};
+	lsm.read();
+	ax = lsm.accelData.x * mg_per_lsb_accel;
+	ay = lsm.accelData.y * mg_per_lsb_accel;
+	az = lsm.accelData.z * mg_per_lsb_accel;
 
-//  uint32_t pixel_duration_us = 1200; // TODO: from gyro
-//  uint32_t rotation_duration_us = 240000;
+	a_norm = sqrt(ax * ax + ay * ay + az * az);
 
-  Serial.println(gz,DEC);
+	gx = (int) lsm.gyroData.x * dps_per_lsb_gyro;
+	gy = (int) lsm.gyroData.y * dps_per_lsb_gyro;
+	gz = (int) lsm.gyroData.z * dps_per_lsb_gyro + gyro_z_offset; // in deg/s
 
-  if(gz > 200)
-  {
-	  uint32_t rotation_duration_us = 1e6 * 360 / gz;
-	  uint32_t pixel_duration_us = 1200; // TODO: from gyro
+	char mystring[] = "TECHFEST";
+
+	if (true)
+	{
+	sprintf(msg, "Gyro %i %i %i", gx, gy, gz);
+	send_msg_via_udp();
+	if (with_serial){ Serial.println(msg); }
+	}
+
+	if (gz > 200) {
+		uint32_t rotation_duration_us = 1e6 * 360 / gz;
+		uint32_t pixel_duration_us = 1200; // TODO: from gyro
 
 //	  uint32_t pixel_duration_us = 1200000; // TODO: from gyro
 //    uint32_t rotation_duration_us = 240000000;
 
-	  uint32_t t_start = micros();
+		uint32_t t_start = micros();
 
-	  for (int i = 0; i < strlen(mystring); i++) {
+		for (int i = 0; i < strlen(mystring); i++) {
 
-		int* mycolor = blue;
+			int* mycolor = blue;
 
-		for (char col = 0; col < 5; col++) {
-		  setChar(mystring[i], col, mycolor);
-		  delayMicroseconds(pixel_duration_us);
+			for (char col = 0; col < 5; col++) {
+				setChar(mystring[i], col, mycolor);
+				delayMicroseconds(pixel_duration_us);
+			}
+			clearChar();
+			delayMicroseconds(2 * pixel_duration_us); // inter-char space
+			Serial.print(mystring[i]);
 		}
-		clearChar();
-		delayMicroseconds(2*pixel_duration_us); // inter-char space
-		Serial.print(mystring[i]);
-	  }
 
-	  uint32_t t_end = micros();
+		uint32_t t_end = micros();
 
-	  delayMicroseconds(rotation_duration_us - (t_end - t_start));
-  }
+		delayMicroseconds(rotation_duration_us - (t_end - t_start));
+	}
 }
